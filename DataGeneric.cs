ac9de4f46b73eea47dd90169b9df10357b498ce7
gector727 <gectorashera@gmail.com>
2026-03-26 13:51:11 +0700

    menambahkan class DataGeneric

---
+++ DataGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmodul5_103082400004
+{
+    internal class DataGeneric<T>
+    {
+        private T data;
+
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: " + data);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TPmodul5_103082400004;
+
+DataGeneric<string> dataNIM = new DataGeneric<string>("103082400004");
+dataNIM.PrintData();
+
+Console.ReadLine();
